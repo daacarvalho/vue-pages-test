9ba27edae2847b44374d15209ae26ba7b179efa2
Daniel Carvalho <danielfccarvalho@hotmail.com>
2024-08-01 08:47:29 -0300

    automatizando deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:daacarvalho/vue-pages-test.git main:gh-pages
+git push -f git@github.com:daacarvalho/vue-pages-test.git master:gh-pages
 
 cd -
